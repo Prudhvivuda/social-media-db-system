cfa6cb93812cba1ff86a08aea3f19cdc52976404
akash6murali <akashmurali150@gmail.com>
2023-12-04 19:47:09 -0500

    delete_api_calls

--- data_operations/delete.py
+++ data_operations/delete.py
@@ -6,12 +6,22 @@ def delete_data():
     cursor = conn.cursor()
 
     data = request.get_json()
-    # id = data['id']
+    # Extract the table name from the data
+    table_name = data['table_name']
 
-    sql_query= "DELETE FROM table_name WHERE id = %s"
-    # data = (id,)
-    # cursor.execute(sql_delete, data)
-    cursor.execute(sql_query)
+    # Remove the table name key-value pair from the data dictionary
+    del data['table_name']
+
+    # Get the field name and value from the data dictionary
+    id_field_name, id_value = next(iter(data.items()))
+
+    # Use a parameterized query to prevent SQL injection
+    sql_delete = "DELETE FROM {} WHERE {} = %s".format(table_name, id_field_name)
+
+   
+    #data = (id,)
+    cursor.execute(sql_delete, (id_value,))
+    # #cursor.execute(sql_delete)
 
     conn.commit()
 
